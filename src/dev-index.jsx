d0b79c12a5e5b9a9661d98158db8f948e4437e9c
breno-helf <breno.helfstein@gmail.com>
2018-03-08 11:49:59 -0300

    using ES6 and lint

--- src/dev-index.jsx
+++ src/dev-index.jsx
@@ -2,151 +2,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactBlocklyComponent from './index';
-const INITIAL_XML = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
-var mathChangeJson = {
-  "type": "payment_method",
-  "message0": "Payment method %1 %2",
-  "args0": [
-    {
-      "type": "field_dropdown",
-      "name": "condition",
-      "options": [
-        [
-          "is",
-          "is"
-        ],
-        [
-          "is not",
-          "is_not"
-        ],
-        [
-          "",
-          ""
-        ]
-      ]
-    },
-    {
-      "type": "field_dropdown",
-      "name": "subject",
-      "options": [
-        [
-          "American Express (1)",
-          "1"
-        ],
-        [
-          "Diners (2)",
-          "2"
-        ],
-        [
-          "Visa (3)",
-          "3"
-        ],
-        [
-          "Mastercard (4)",
-          "4"
-        ],
-        [
-          "Boleto (6)",
-          "6"
-        ],
-        [
-          "Tarjeta Cencosud (501)",
-          "501"
-        ]
-      ]
-    }
-  ],
-  "inputsInline": true,
-  "previousStatement": "logic_type",
-  "nextStatement": "Boolean",
-  "colour": 230,
-  "tooltip": "Will evaluate to true if the user is buying with the desired payment method",
-  "helpUrl": ""
-};
+
+const INITIAL_XML = '<xml xmlns="http://www.w3.org/1999/xhtml"></xml>';
+const conditionBlocks = require('../resources/blocks.json');
+
 const CATEGORIES = [{
-  name: 'Test',
-  blocks: [{type: 'test'}]
+  name: 'Conditional Prices',
+  blocks: [],
 }];
+
 function createBlocks() {
-  var conditionBlocks = require('../resources/blocks.json');
   console.log(conditionBlocks);
-  var types = [];
-  conditionBlocks.forEach(element => {
-    var type = element['type'];
+  const types = [];
+  conditionBlocks.forEach((element) => {
+    const { type } = element;
     types.push(type);
     Blockly.Blocks[type] = {
-      init: function() {
+      init() {
         this.jsonInit(element);
-        // Assign 'this' to a variable for use in the tooltip closure below.
-        var thisBlock = this;
-        this.setTooltip(function() {
-          // return 'Add a number to variable "%1".'.replace('%1',
-          //     thisBlock.getFieldValue('VAR'));
-        });
-      }
-    };   
+      },
+    };
   });
-  //Extract to another function later on
-  types.forEach(element => {
-      var block = {type: element};
-      CATEGORIES[0]['blocks'].push(block);
+  // Extract to another function later on
+  types.forEach((element) => {
+    const block = { type: element };
+    CATEGORIES[0].blocks.push(block);
   });
-} 
+}
+
 createBlocks();
-const INITIAL_TOOLBOX_CATEGORIES = [
-  {
-    name: 'Controls',
-    blocks: [
-      { type: 'math_change'},
-      { type: 'controls_if' },
-      {
-        type: 'controls_repeat_ext',
-        values: {
-          TIMES: {
-            type: 'math_number',
-            shadow: true,
-            fields: {
-              NUM: 10,
-            },
-          },
-        },
-        statements: {
-          DO: {
-            type: 'text_print',
-            shadow: true,
-            values: {
-              TEXT: {
-                type: 'text',
-                shadow: true,
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
-        },
-      },
-    ],
-  },
-  {
-    name: 'Text',
-    blocks: [
-      { type: 'text' },
-      {
-        type: 'text_print',
-        values: {
-          TEXT: {
-            type: 'text',
-            shadow: true,
-            fields: {
-              TEXT: 'abc',
-            },
-          },
-        },
-      },
-    ],
-  },
-];
+
 class TestEditor extends React.Component {
   constructor(props) {
     super(props);
@@ -154,93 +39,69 @@ class TestEditor extends React.Component {
       toolboxCategories: CATEGORIES,
     };
   }
-  componentDidMount = () => {
-    window.setTimeout(() => {
-      this.setState({
-        toolboxCategories: this.state.toolboxCategories.concat([
-          {
-            name: 'Text2',
-            blocks: [
-              { type: 'text' },
-              {
-                type: 'text_print',
-                values: {
-                  TEXT: {
-                    type: 'text',
-                    shadow: true,
-                    fields: {
-                      TEXT: 'abc',
-                    },
-                  },
-                },
-              },
-            ],
-          },
-        ]),
-      });
-    }, 2000);
-  }
 
   defineBlockTranslations = () => {
     console.log('defining blocks');
     Blockly.JavaScript.payment_method = (block) => {
       const dropdownCondition = block.getFieldValue('condition');
       const dropdownSubject = block.getFieldValue('subject');
-      const connector = dropdownCondition == 'is' ? '==' : '!=';
+      const connector = dropdownCondition === 'is' ? '==' : '!=';
 
       const code = `(first (.params[]? | select(.name == "paymentMethodId")) | (.value | tonumber) ${connector} ${dropdownSubject})`;
       return code;
     };
 
-    Blockly.JavaScript.logic_and = (block) => {
-      const code = ` and `;
-      return code;
-    };
-    
-    Blockly.JavaScript.logic_or = (block) => {
-      const code = ` or `;
+    Blockly.JavaScript.logic_and = () => {
+      const code = ' and ';
       return code;
     };
 
-    Blockly.JavaScript.logic_not = (block) => {
-      const code = ` not `;
+    Blockly.JavaScript.logic_or = () => {
+      const code = ' or ';
+      return code;
+    };
+
+    Blockly.JavaScript.logic_not = () => {
+      const code = ' not ';
       return code;
     };
 
     Blockly.JavaScript.payment_bin = (block) => {
       const dropdownCondition = block.getFieldValue('condition');
-      const bin_max = block.getFieldValue('bin_max');
-      const bin_min = block.getFieldValue('bin_min');
+      const binMax = block.getFieldValue('bin_max');
+      const binMin = block.getFieldValue('bin_min');
 
-      const connector = dropdownCondition == 'between' ? '' : 'not ';
-      console.log(bin_min)
-      
-      const code = `(first (.params[]? | select(.name == "restrictionsBins")) | ${connector} ((.value | tonumber) >= ${bin_min}) and (first(.params[]? | select(.name == "restrictionsBins")) | (.value | tonumber) <= ${bin_max}))`;
+      const connector = dropdownCondition === 'between' ? '' : 'not ';
+      console.log(binMin);
+
+      const code = `(first (.params[]? | select(.name == "restrictionsBins")) | ${connector} ((.value | tonumber) >= ${binMin}) and (first(.params[]? | select(.name == "restrictionsBins")) | (.value | tonumber) <= ${binMax}))`;
       return code;
     };
 
     Blockly.JavaScript.user_utm = (block) => {
       const dropdownCondition = block.getFieldValue('condition');
-      const utm_type = block.getFieldValue('utm_type')
-      const dropdownSubject = block.getFieldValue('subject')
+      const utmType = block.getFieldValue('utm_type');
+      const dropdownSubject = block.getFieldValue('subject');
 
-      var code = '';
-      if (dropdownCondition == 'is' || dropdownCondition == 'is_not') {
-        const connector = dropdownCondition == 'is' ? '==' : '!=';
-        code = `(.${utm_type} ${connector} "${dropdownSubject}")`;
+      let code = '';
+      if (dropdownCondition === 'is' || dropdownCondition === 'is_not') {
+        const connector = dropdownCondition === 'is' ? '==' : '!=';
+        code = `(.${utmType} ${connector} "${dropdownSubject}")`;
       }
-      if (dropdownCondition == 'contains' || dropdownCondition == 'does_not_contain') {
-        const connector = dropdownCondition == 'contains' ? `| contains('` : `| not (contains('`;
-        const closePar = dropdownCondition == 'contains' ? `')` : `'))`;
-        code = `(.${utm_type} ${connector}${dropdownSubject}${closePar})`
-      } 
+
+      if (dropdownCondition === 'contains' || dropdownCondition === 'does_not_contain') {
+        const connector = dropdownCondition === 'contains' ? '| contains(\'' : '| not (contains(\'';
+        const closePar = dropdownCondition === 'contains' ? '\')' : '\'))';
+        code = `(.${utmType} ${connector}${dropdownSubject}${closePar})`;
+      }
+
       return code;
-    };    
+    };
 
     Blockly.JavaScript.user_firstbuy = (block) => {
       const dropdownCondition = block.getFieldValue('condition');
-      const connector = dropdownCondition == 'is' ? '==' : '!=';
-      
+      const connector = dropdownCondition === 'is' ? '==' : '!=';
+
       const code = `(.isFirstBuy ${connector} true)`;
       return code;
     };
